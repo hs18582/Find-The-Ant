6608b697c5680d0e459fb6dd4504626d902427ef
Hamsini Shivakumar <hs18582@essex.ac.uk>
2022-01-17 15:40:57 +0000

    Create and Draw buttons for the main menu

--- FindTheAnt.py
+++ FindTheAnt.py
@@ -1,7 +1,17 @@
 from Entity import *
 from pygame.locals import *
+import sys
 
 
+
+class Game:
+    pass
+########################################################################################################################
+class Rules:
+    pass
+########################################################################################################################
+class HighScore:
+    pass
 ########################################################################################################################
 
 def main_menu():
@@ -11,15 +21,51 @@ def main_menu():
 
     while True:
 
-        screen.fill(black)
+        screen.fill((0, 0, 0))
 
         writeText("Find The Ant", fontTitle, brown, screen, 3 * screen.get_width() / 4, screen.get_height() / 20)
+        mx, my = pygame.mouse.get_pos()
 
         click = False
         for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
+
+        Start = pygame.Rect(screen.get_width() / 20, screen.get_height() / 8, screen.get_width() / 2,
+                            screen.get_height() / 8)
+        Instruction = pygame.Rect(screen.get_width() / 20, (screen.get_height() / 8 + (screen.get_height() / 4)),
+                                  (3 * screen.get_width()) / 8, screen.get_height() / 8)
+        Highscore = pygame.Rect(screen.get_width() / 20, (screen.get_height() / 8 + (screen.get_height() / 2)),
+                                (screen.get_width()) / 4, screen.get_height() / 8)
+        Quit = pygame.Rect(screen.get_width() / 20, (screen.get_height() / 8 + (3 * screen.get_height() / 4)),
+                           (screen.get_width()) / 8, screen.get_height() / 8)
+
+        if Start.collidepoint((mx, my)):
+            if click:
+                Game()
+        if Instruction.collidepoint((mx, my)):
+            if click:
+                Rules()
+        if Highscore.collidepoint((mx, my)):
+            if click:
+                HighScore()
+        if Quit.collidepoint((mx, my)):
+            if click:
+                pygame.quit()
+                sys.exit()
+
+        pygame.draw.rect(screen, green, Start)
+        writeText("Start", fontSmall, brown, screen, screen.get_width() / 10, screen.get_height() / 6)
+
+        pygame.draw.rect(screen, brown, Instruction)
+        writeText("Rules", fontSmall, green, screen, screen.get_width() / 10, screen.get_height() / 2.4)
+
+        pygame.draw.rect(screen, green, Highscore)
+        writeText("Highscore", fontSmall, brown, screen, screen.get_width() / 7.5, screen.get_height() / 1.5)
+
+        pygame.draw.rect(screen, brown, Quit)
+        writeText("Exit", fontSmall, green, screen, screen.get_width() / 11, screen.get_height() / 1.1)
 
         pygame.display.update()
         mainClock.tick(60)
