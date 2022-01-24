c5f38b28c9b06a7f61e88de96a42d6c222091942
Hamsini Shivakumar <hs18582@essex.ac.uk>
2022-01-24 14:12:07 +0000

    Creating a green grid, N has not be defined yet

--- FindTheAnt.py
+++ FindTheAnt.py
@@ -2,12 +2,17 @@ from Entity import *
 from pygame.locals import *
 import sys, random
 
+
 class Init:
     def __init__(self):
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.running = True
         self.mainClock = pygame.time.Clock()
+
+
 Init = Init()
+
+
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -15,16 +20,39 @@ def Game():
     pygame.init()
     while Init.running:
         Init.screen.fill(black)
+        list_buttons = Grid()
 
     pygame.display.update()
     Init.mainClock.tick(60)
 
+
+def Grid():
+    # CREATING A LIST USING LIST COMPREHENSION
+    list_buttons = [
+        [pygame.Rect(((Init.width / Init.getN()) * i) + ((Init.screen.get_width() / 2) - 300),
+                     ((Init.height / Init.getN()) * j) + ((Init.screen.get_height() / 2) - 300),
+                     (Init.width / Init.getN()) - 2,
+                     (Init.height / Init.getN()) - 2) for i in
+         range(Init.getN())] for j in range(Init.getN())]
+
+    # LOOP TO CREATE A N BY N GRID
+    for i in range(len(list_buttons)):
+        for j in range(len(list_buttons[i])):
+            pygame.draw.rect(Init.screen, Init.colour[i][j], list_buttons[i][j])  # CREATING A GREEN RECTANGLE
+
+    return list_buttons
+
+
 ########################################################################################################################
 class Rules:
     pass
+
+
 ########################################################################################################################
 class HighScore:
     pass
+
+
 ########################################################################################################################
 def main_menu():
     mainClock = pygame.time.Clock()
