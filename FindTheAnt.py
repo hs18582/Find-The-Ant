fbdc2be1035a1f3f7c46a881aaa1779d9ccf94ba
hs18582 <hs18582@essex.ac.uk>
2022-02-01 14:30:16 +0000

    Adding buttons and calling playagain method to restart the game

--- FindTheAnt.py
+++ FindTheAnt.py
@@ -252,7 +252,42 @@ def Loser():
     LoserRunning = True
     antmini = pygame.image.load('Images/antMini.png')  # https://www.pinclipart.com/maxpin/iTbwbmJ/
 
+    while LoserRunning:
+        screen.fill((0, 0, 0))
 
+        writeText("Time's Up!", fontTitle, (255, 255, 255), screen, screen.get_width() / 2, screen.get_height() / 20)
+        writeText("Sorry you didnt find the ant in time! ", fontTitle, (255, 255, 255), screen, screen.get_width() / 2,
+                  8 * screen.get_height() / 20)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        # Creating and Drawing back button
+        mx, my = pygame.mouse.get_pos()
+        Back = pygame.Rect(3 * screen.get_width() / 4, 7 * screen.get_height() / 10, screen.get_width() / 4,
+                           screen.get_height() / 10)
+        if Back.collidepoint((mx, my)):
+            if click:
+                pygame.time.delay(500)
+                main_menu()
+        pygame.draw.rect(screen, green, Back)
+        writeText("Back", fontSmall, brown, screen, 4 * screen.get_width() / 5, 14.5 * screen.get_height() / 20)
+
+        Reset = pygame.Rect(0 * screen.get_width(), 7 * screen.get_height() / 10, screen.get_width() / 4,
+                            screen.get_height() / 10)
+        if Reset.collidepoint((mx, my)):
+            if click:
+                pygame.time.delay(500)
+                playAgain()
+                Game()
+        pygame.draw.rect(screen, green, Reset)
+        writeText("Play Again?", fontSmall, brown, screen, 1 * screen.get_width() / 10, 14.5 * screen.get_height() / 20)
+
+        pygame.display.update()
+        mainClock.tick(60)
 
 
 ########################################################################################################################
